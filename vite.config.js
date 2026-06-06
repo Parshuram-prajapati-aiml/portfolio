3f6a7c9af44a3dd9b54a7d78fb56b16ca283d129
parshuram-prajapati <parshuram8792@gmail.com>
2026-06-06 17:07:30 +0530

    Fix Render host issue

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
 
+  server: {
+    host: '0.0.0.0'
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: [
+      'portfolio-ya44.onrender.com'
+    ]
+  }
+})
